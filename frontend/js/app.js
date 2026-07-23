d2145a90afa9389a182b0ccf8f53ea21c646f98f
truongminhnhut15938-del <truongminhnhut15938@gmail.com>
2026-07-23 15:30:47 +0700

    BUILD-0.1.0: Initial project structure and UI

---
+++ frontend/js/app.js
@@ -0,0 +1,8 @@
+document.getElementById("searchBtn").addEventListener("click",()=>{
+
+    const keyword=document.getElementById("searchInput").value;
+
+    alert("Bạn vừa tìm: "+keyword);
+
+    });
+})
