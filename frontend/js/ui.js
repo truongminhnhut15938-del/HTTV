e8f5f43438b56904e89376acb6e6ad65f046399e
truongminhnhut15938-del <truongminhnhut15938@gmail.com>
2026-07-29 09:13:17 +0700

    Update ui.js

--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -1,1 +1,21 @@
-console.log("UI Engine loaded.");
+const content = document.getElementById("content");
+
+// Hiển thị thông tin file
+function showFileInfo(file, text = "") {
+
+    const sizeKB = (file.size / 1024).toFixed(2);
+
+    content.innerHTML = `
+        <div class="document-card">
+
+            <h2>📄 ${file.name}</h2>
+
+            <p><b>Kích thước:</b> ${sizeKB} KB</p>
+
+            <hr>
+
+            <pre class="document-content">${text}</pre>
+
+        </div>
+    `;
+}
