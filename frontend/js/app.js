9ba01bced269d355795d14f150af06b10efd300d
truongminhnhut15938-del <truongminhnhut15938@gmail.com>
2026-07-29 10:29:10 +0700

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,22 +1,28 @@
 const btnUpload = document.getElementById("btnUpload");
 const fileInput = document.getElementById("fileInput");
 const content = document.getElementById("content");
+
 btnUpload.addEventListener("click", openFileDialog);
 fileInput.addEventListener("change", handleFileSelected);
+
 function openFileDialog() {
-    fileInput.click();
+fileInput.click();
 }
+
 async function handleFileSelected() {
-    if (fileInput.files.length === 0) {
-        return;
-    }
-    const file = fileInput.files[0];
-    try {
-        content.innerHTML = "Đang đọc tài liệu...";
-        const text = await readDocument(file);
-        showFileInfo(file, text);
-    } catch (error) {
-        console.error(error);
-        showFileInfo(file, "Không thể đọc tài liệu này.\n\n" + error.message);
-    }
+if (fileInput.files.length === 0) {
+return;
+}
+
+const file = fileInput.files[0];
+
+try {
+    content.innerHTML = "Đang đọc tài liệu...";
+    const text = await readDocument(file);
+    showFileInfo(file, text);
+} catch (error) {
+    console.error(error);
+    showFileInfo(file, "Không thể đọc tài liệu này.\n\n" + error.message);
+}
+
 }
