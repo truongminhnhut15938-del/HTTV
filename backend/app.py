3b30f73cb46bca62326a2857404ccd0ff7ad3c11
truongminhnhut15938-del <truongminhnhut15938@gmail.com>
2026-07-30 14:36:32 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(name)
 
 @app.route('/health')
 def health():
-    return {'status': 'HTTV OCR Backend OK'}
+return {'status': 'HTTV OCR Backend OK'}
 
 if name == 'main':
 app.run(host='0.0.0.0', port=5000, debug=True)
